5d56ca760b209913e60d683e1c9bfb4623166ffa
jeongyong <cin03023@gmail.com>
2025-05-20 23:39:07 +0900

    주석 추가

--- src/test/java/com/junit/demo/service/BookServiceTest.java
+++ src/test/java/com/junit/demo/service/BookServiceTest.java
@@ -16,6 +16,7 @@ public class BookServiceTest {
     @Autowired
     private BookRepository bookRepository;
 
+    // 문제점 --> service 만 테스트하고 싶은데, repository 까지 함께 테스트되고 있음
     @Test
     public void 책등록하기() {
         // given
